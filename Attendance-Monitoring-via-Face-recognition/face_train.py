4e1df85ef319b0ec305fe12b1357f45de3f9fb40
Yash Joshi <121224573+yashjoshi5328@users.noreply.github.com>
2023-12-08 05:45:25 +0530

    Update face_train.py
    deployed front end using tkinter

--- Attendance-Monitoring-via-Face-recognition/face_train.py
+++ Attendance-Monitoring-via-Face-recognition/face_train.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import os
 import csv 
+from tkinter import *
 
 f=open("student_record.csv",'a',newline='')
 csvwriter = csv.writer(f)
@@ -9,47 +10,7 @@ csvwriter = csv.writer(f)
 
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-def face_cap():
 
-    cap = cv2.VideoCapture(0)
-
-    ID = input("Enter your ID: ")
-
-    Name = input("Enter your name: ")
-    
-    csvwriter.writerow([ID,Name])
-    
-    f.close()
-
-    sampleNum = 0
-
-    while (True):
-
-        ret,img = cap.read()
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_detector.detectMultiScale(gray, 1.3, 5)
-
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-            # incrementing sample number
-            sampleNum = sampleNum + 1
-
-            # saving the captured face in the face_data folder with imwrite("name with path",image)
-            cv2.imwrite("face_data/ " + Name + "." + ID + '.' + str(sampleNum) + ".jpg", gray[y:y + h, x:x + w])
-
-            cv2.imshow('Frame',img)
-
-            # wait for 100 miliseconds
-        if cv2.waitKey(1)== 13 :
-            break
-            # break if the sample number is morethan 1000
-        elif sampleNum > 1000:
-            break
-
-    print("\nFACE DATA SAVED FOR {0} , ID : {1}".format(Name,ID))   
-    cap.release()
-    cv2.destroyAllWindows() 
 def get_img_and_id(path):
     # create empth face list
     faceSamples = []
@@ -91,7 +52,77 @@ def get_img_and_id(path):
            
     return faceSamples, Ids
 
-        
+
+#face_cap()     
+#train_model()
+window=Tk() #initiating a instance of a window 
+
+icon=PhotoImage(file='graphic.png')
+window.iconphoto(True,icon)
+
+window.geometry('840x420')
+
+window.title('FACE TRAIN SYSTEM')
+window.config(background='#D2B4DE')
+
+label1=Label(window,text='FACE TRAIN SYSTEM',font=('Arial',30,'bold','underline'),fg='black',bg='#D2B4DE')
+label1.pack()
+
+label2=Label(window,text='Name',font=('Arial',20,'bold'),fg='black',bg='#D2B4DE')
+label2.place(x=0,y=100)
+entry1=Entry(window,font=('Arial',20,'bold'),fg='black')
+entry1.place(x=100,y=100)
+
+label3=Label(window,text='ID',font=('Arial',20,'bold'),fg='black',bg='#D2B4DE')
+label3.place(x=0,y=140)
+entry2=Entry(window,font=('Arial',20,'bold'),fg='black')
+entry2.place(x=100,y=140)
+
+def face_cap():
+
+    cap = cv2.VideoCapture(1)
+    cap.set(cv2.CAP_PROP_BUFFERSIZE, 3);
+
+    ID = entry2.get()
+
+    Name = entry1.get()
+    
+    csvwriter.writerow([ID,Name])
+
+    sampleNum = 0
+
+    while (True):
+
+        ret,img = cap.read()
+        if(ret):
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+                # incrementing sample number
+                sampleNum = sampleNum + 1
+
+                # saving the captured face in the face_data folder with imwrite("name with path",image)
+                cv2.imwrite("face_data/ " + Name + "." + ID + '.' + str(sampleNum) + ".jpg", gray[y:y + h, x:x + w])
+
+                cv2.imshow('Frame',img)
+
+            # wait for 100 miliseconds
+        if cv2.waitKey(1)== 13 :
+            break
+            # break if the sample number is more than 1000
+        elif sampleNum > 150:
+            break
+
+    print("\nFACE DATA SAVED FOR {0} , ID : {1}".format(Name,ID))   
+    cap.release()
+    cv2.destroyAllWindows() 
+
+button1=Button(window,text='SUBMIT',command=face_cap,font=('Comic Sans',15),activebackground='RED')
+button1.place(x=370,y=200)
+
 def train_model():
 
     recognizer= cv2.face.LBPHFaceRecognizer_create()
@@ -104,7 +135,9 @@ def train_model():
 
     print("MODEL TRAINED")
 
+button2=Button(window,text='UPDATE MODEL',command=train_model,font=('Comic Sans',15),activebackground='RED')
+button2.place(x=330,y=250)
 
-face_cap()     
-train_model()
+window.mainloop() #display window 
 
+f.close()
